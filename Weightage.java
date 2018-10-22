6293de6bc3d4eab49c3a190a2843582d078f4786
Maggie Koesno <maggiekoesno@Maggies-MacBook-Pro.local>
2018-10-22 23:48:53 +0800

    Fix Weightage bugs

--- Weightage.java
+++ Weightage.java
@@ -3,30 +3,30 @@ public class Weightage{
     private double value;
     private Weightage parent;
     public Weightage(String name, double value){
-        name = name;
-        value = value;
+        this.name = name;
+        this.value = value;
     }
     public Weightage(String name, double value, Weightage parent){
-        name = name;
-        value = value;
-        parent = parent;
+        this.name = name;
+        this.value = value;
+        this.parent = parent;
     }
     public String getName(){
         return name;
     }
     public void setName(String name){
-        name = name;
+        this.name = name;
     }
     public double getValue(){
         return value;
     }
     public void setValue(double value){
-        value = value;
+        this.value = value;
     }
     public Weightage getParent(){
         return parent;
     }
-    public Weightage setParent(){
-        parent = parent;
+    public void setParent(Weightage parent){
+        this.parent = parent;
     }
 }
