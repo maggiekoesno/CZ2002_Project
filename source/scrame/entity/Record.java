06b894a57eaab8319dee431d3aadebf86585e1d7
kevinwinatamichael <kevinwinatamichael@gmail.com>
2018-10-30 19:48:45 +0800

    Done initial implementation for Record

--- source/scrame/entity/Record.java
+++ source/scrame/entity/Record.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 
 import scrame.entity.Student;
 import scrame.entity.Course;
-import scrame.helper.Mark;
 
 public class Record implements Serializable {
   private Student student;
@@ -52,13 +51,13 @@ public class Record implements Serializable {
   private float calculateAverage(String check){
     float sum=0;
     for (Map.Entry<String, String[]> entry : course.getWeightage().entrySet()){
-      if(entry.getValue()[HAS_CHILD]=="false" && entry.getValue()[PARENT].equals(check)){
+      if(entry.getValue()[PARENT].equals(check)){
+        if(entry.getValue()[HAS_CHILD].equals("true")){
+          sum += calculateAverage(entry.getKey());
+        }
         String w = entry.getValue()[WEIGHT];
-        float val = 0.01*Integer.parseInt(w.substring(0, w.length() - 1));
+        float val = (float)0.01*Integer.parseInt(w.substring(0, w.length() - 1));
         sum += mark.get(entry.getKey())*val;
-      }
-      else if(entry.getValue()[HAS_CHILD]=="true"){
-        sum += calculateAverage(entry.getKey());
       }
     }
     return sum;
@@ -76,4 +75,39 @@ public class Record implements Serializable {
     return mark;
   }
 
+  public static void main(String[] args){
+    try {
+            HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+            Course c1 = new Course("CZ2001", CourseType.LEC);
+            map.put("_LEC", 50);
+            c1.addGroups(map);
+            HashMap<String, String[]> weightage = new HashMap<String, String[]>();
+            weightage.put("exam", new String[] { "60%", "false", "" });
+            weightage.put("coursework", new String[] { "40%", "true", "" });
+            weightage.put(
+                "assigments",
+                new String[] { "70%", "false", "coursework" }
+            );
+            weightage.put(
+                "attendance",
+                new String[] { "30%", "false", "coursework" }
+            );
+            c1.setWeightage(weightage);
+            
+            Student tester = new Student("Alice", "ACC", "AY1718 S1", "U1723456A");
+
+            HashMap<String, Float> markIn = new HashMap<String, Float>();
+            markIn.put("exam", (float)80);
+            markIn.put("assigments", (float)70);
+            markIn.put("attendance", (float)90);
+            Record rec = new Record(tester, c1, markIn);
+            System.out.println("Here it comes");
+            float res = rec.calculateAverage();
+            System.out.println(res);
+
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+  }
 }
