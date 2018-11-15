563749aa1b6c713d6ef598c42fcd22cc10edb7e3
kevinwinatamichael <kevinwinatamichael@gmail.com>
2018-11-15 18:15:22 +0800

    Done javadoc for helper classes

--- source/scrame/helper/CourseType.java
+++ source/scrame/helper/CourseType.java
@@ -1,10 +1,20 @@
 package scrame.helper;
 
+/**
+ * Course type that can be used
+ */
 public enum CourseType {
-
+  /**
+   * Course that has Lecture only
+   */
   LEC,
+  /**
+   * Course that has Lecture and Tutorial
+   */
   TUT,
+  /**
+   * Course that has lecture, tutorial and lab
+   */
   LAB;
-
 }
 
--- source/scrame/helper/SortByCourseName.java
+++ source/scrame/helper/SortByCourseName.java
@@ -4,7 +4,14 @@ import java.util.Comparator;
 
 import scrame.entity.Course;
 
+/**
+ * A helper class that help to compare 2 courses
+ */
 public class SortByCourseName implements Comparator<Course> {
+  /**
+   * A function that compare 2 course objects
+   * @return integer that return the relation of the 2 courses' name
+   */
   public int compare(Course a, Course b) {
     return a.getCourseName().compareTo(b.getCourseName());
   }
